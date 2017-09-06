b82396a8ac723469110c8e9e02c102c5be68902e
Bartosz Nowak <bartosz.nowak@globallogic.com>
2017-09-06 10:52:50 +0300

    Retrieved data from spring

--- src/TestApp.js
+++ src/TestApp.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import NavBar from './components/navBar/NavBar';
 import TaskList from './components/taskList/TaskList';
+import $ from 'jquery'; 
+
+window.onload = function()
+{
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function()
+    {
+        if(http.readyState === 4 && http.status === 200)
+        {
+            console.log(JSON.parse(http.response));
+        }
+    }
+
+    http.open("GET", "http://localhost:8080/tasks", true);
+    http.send();
+
+    $.get("http://localhost:8080/tasks", function(data) {
+        console.log(data);
+    });
+}
 
 export default class Test extends React.Component 
 {
