8c076684f228dd9ae5ac1db6a0f7a1434d28b8ee
Bartosz Nowak <bartosz.nowak@globallogic.com>
2017-09-13 17:35:37 +0300

    Changed numbers format in task window

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -14,6 +14,38 @@ export default class Task extends React.Component
         TaskActions.deleteTask(this.props.taskModel.id);
     }
 
+    //refactor this please..
+    taskFormatNumbers()
+    {
+        var numbers = this.props.taskModel.taskData.numbers;
+        var result = "";
+        var sign;
+        switch(this.props.taskModel.taskData.operation)
+        {
+            case "ADD":
+            sign = " + ";
+            break;
+            case "SUBTRACT":
+            sign = " - ";
+            break;
+            case "MULTIPLY":
+            sign = " * ";
+            break;
+            case "DIVIDE":
+            sign = " / ";
+            break;
+            default:
+            sign = " ? ";
+        }
+        if(numbers.length === 0) return sign;
+        for(var i = 0; i < numbers.length - 1; i++)
+        {
+            result += numbers[i] + sign;
+        }
+        result += numbers[numbers.length - 1];
+        return result;
+    }
+
     render()
     {
         console.log(this.props);
@@ -22,7 +54,7 @@ export default class Task extends React.Component
             <div className = "Task">
                 <p>Task: {this.props.taskModel.id}</p>
                 <p>{this.props.taskModel.description}</p>
-                <p>{this.props.taskModel.taskData.numbers} {this.props.taskModel.taskData.operation}</p>
+                <p>{this.taskFormatNumbers()}</p>
                 <p>Result: {this.props.taskModel.result.value}</p>
                 <button onClick={this.executeTask.bind(this)}>
                     Execute
